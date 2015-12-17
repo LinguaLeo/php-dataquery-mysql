a2e73e5e4ac087324c649cfcfb35af7f6949d080
Vladimir Alexeev <vealexeev@lingualeo.com>
2015-12-17 13:41:05 +0300

    DbConnectableInterface namespace use fixes

--- src/LinguaLeo/MySQL/Interfaces/DbConnectableInterface.php
+++ src/LinguaLeo/MySQL/Interfaces/DbConnectableInterface.php
@@ -1,13 +1,16 @@
 <?php
 namespace LinguaLeo\MySQL\Interfaces;
 
+use LinguaLeo\DataQuery\Criteria;
+use LinguaLeo\MySQL\Connection;
+
 interface DbConnectableInterface
 {
     /**
      * Returns actual DB connection
      *
      * @param Criteria $criteria
-     * @return \PDO
+     * @return Connection
      */
     public function getConnection(Criteria $criteria);
 }
